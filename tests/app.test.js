27168b10a61f4009bf0554303e2d2136d374caa0
Ammar <ammar.y.2667@gmail.com>
2025-03-25 11:31:36 +0500

    Remove test cases and add initialization logs in app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,20 +1,3 @@
-// tests/app.test.js
-const request = require('supertest');
-const app = require('../server');
-const { users, events } = require('../data');
+console.log('Test run initialized')
 
-describe('Auth API', () => {
-  // Clear users and events arrays after each test
-  afterEach(() => {
-    users.length = 0;
-    events.length = 0;
-  });
-
-  it('should register a new user', async () => {
-    const res = await request(app)
-      .post('/auth/register')
-      .send({ username: 'testuser', password: 'testpass' });
-    expect(res.statusCode).toEqual(201);
-    expect(res.body.message).toBe('User registered successfully');
-  });
-});
+console.log('Test run complete')
